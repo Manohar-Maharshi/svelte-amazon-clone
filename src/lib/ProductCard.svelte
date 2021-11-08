2c4fdaeef55ea5874663bc3bb65987eb6baddcec
Manohar-Maharshi <netwitch101@gmail.com>
2021-11-08 19:31:30 +0530

    🧾dummy products from "fakestoreapi"
    > displayed based on "categories"

---
+++ src/lib/ProductCard.svelte
@@ -0,0 +1,37 @@
+<script>
+	export let item;
+</script>
+<div class="m-1 my-3 mx-4 shadow-lg border border-gray-300 rounded-lg w-[18rem] min-h-[25rem]  max-h-[30rem] flex flex-col items-center p-1 px-2 pt-2">
+	<div>
+		<img class="w-full max-w-[16rem] min-h-[11rem] max-h-[7rem] object-cover" src={item?.image} alt={item?.title}>
+	</div>
+	<div class="my-1 flex-1 w-full	">
+		<h2 class="font-semibold leading-tight mt-1 line-clamp-2">{item?.title}</h2>
+		<p class="text-2xl font-semibold my-2"><small>$</small>{item?.price}</p>
+		<p class="mb-1">
+			<span class="text-sm">Rating:</span>
+			<span class="text-sm">({Math.round(item?.rating?.rate)})</span>
+			{#each Array(Math.round(item?.rating?.rate)).fill() as _, index}
+				<span>⭐</span>
+			{/each}
+			<span class="text-xs italic">({item?.rating?.count})</span>
+		</p>
+	</div>
+	<div class="w-full flex flex-col items-center space-y-1 mt-1 mb-1">
+		<div class="flex items-center justify-between space-x-1 w-full text-center text-sm">
+			<button class="text-xs font-bold w-full border bg-[#ffa41c] py-1.5 flex items-center justify-center">
+				<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+				  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+				</svg>
+				<span>Add to Wishlist</span>
+			</button>
+			<button class="text-xs font-bold w-full border bg-[#ffa41c] py-1.5 flex items-center justify-center">
+				<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" >
+					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"/>
+				</svg>
+				<span>Add to Cart</span>
+			</button>
+		</div>
+		<button class="w-full py-1.5 shadow-lg bg-[#e47911] font-bold">Buy Now</button>
+	</div>
+</div>
--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,1 +1,34 @@
-Home
+<script>
+	import  ProductCard from '$lib/ProductCard.svelte';
+	import  Spinner from '$lib/Spinner.svelte';
+
+	const fetchCategories = async () => {
+		const res = await fetch("https://fakestoreapi.com/products/categories");
+		return (await res.json());
+	}
+
+	const fetchProductsByCategory = async (item) => {
+		const res = await fetch(`https://fakestoreapi.com/products/category/${item}`);
+		return (await res.json());
+	}
+
+
+</script>
+
+{#await fetchCategories()}
+	<Spinner />
+{:then result}
+		{#each result as item, index}
+			{#await fetchProductsByCategory(item)}
+			{:then result}
+				<div class="container mx-auto my-10 mt-5">
+					<p class="font-semibold border-b-2 border-yellow-700 inline-block capitalize text-2xl py-1">{item}</p>
+					<div class="flex items-start flex-wrap">	
+						{#each result as item (item.id)}
+							<ProductCard {item} />
+						{/each}
+					</div>
+				</div>
+			{/await}
+		{/each}
+{/await}
