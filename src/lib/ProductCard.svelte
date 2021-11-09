fb87bfbab03ea51ff4fca187b36591eb4634e896
Manohar-Maharshi <netwitch101@gmail.com>
2021-11-09 21:33:51 +0530

    🤩complete mock check out functionality DONE

--- src/lib/ProductCard.svelte
+++ src/lib/ProductCard.svelte
@@ -1,5 +1,7 @@
 <script>
 	import { cartList,wishList } from '$store/store'
+	import { navigate } from "svelte-routing";
+
 	export let item;
 
 
@@ -12,6 +14,17 @@
 		   }
   		$cartList = [...$cartList, productInfo]
 	}
+
+	const directBuy = (productInfo) => {
+		  for(let item of $cartList) {
+		    if(item.id === productInfo.id) {
+				alert("Waite a minute")
+				return;
+		     }
+		   }
+  		$cartList = [...$cartList, productInfo];
+  		navigate('/cart');
+	}
 	const addToWishList = (productInfo) => {
 		  for(let item of $wishList) {
 		    if(item.id === productInfo.id) {
@@ -21,6 +34,7 @@
 		   }
   		$wishList = [...$wishList, productInfo]
 	}
+
 
 
 </script>
@@ -43,19 +57,19 @@
 	</div>
 	<div class="w-full flex flex-col items-center space-y-1 mt-1 mb-1">
 		<div class="flex items-center justify-between space-x-1 w-full text-center text-sm">
-			<button  on:click={() => addToWishList({...item,quantity:1})} class="text-xs font-bold w-full border bg-[#ffa41c] py-1.5 flex items-center justify-center">
+			<button  on:click={() => addToWishList({...item,quantity:1})} class="active:transform active:scale-90 text-xs font-bold w-full border bg-[#ffa41c] py-1.5 flex items-center justify-center">
 				<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 				  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
 				</svg>
 				<span>Add to Wishlist</span>
 			</button>	
-				<button on:click={() => addToCart({...item,quantity:1})} class="text-xs font-bold w-full border bg-[#ffa41c] py-1.5 flex items-center justify-center">
-					<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" >
-						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"/>
-					</svg>
-					<span>Add to Cart</span>
-				</button>	
+			<button on:click={() => addToCart({...item,quantity:1})} class="active:transform active:scale-90 text-xs font-bold w-full border bg-[#ffa41c] py-1.5 flex items-center justify-center">
+				<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" >
+					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"/>
+				</svg>
+				<span>Add to Cart</span>
+			</button>	
 		</div>
-		<button class="w-full py-1.5 shadow-lg bg-[#e47911] font-bold">Buy Now</button>
+		<button on:click={() => directBuy({...item,quantity:1})} class="active:transform active:scale-90 w-full py-1.5 shadow-lg bg-[#e47911] font-bold">Buy Now</button>
 	</div>
 </div>
--- src/routes/Cart.svelte
+++ src/routes/Cart.svelte
@@ -1,13 +1,38 @@
 <script>
+	import * as animateScroll from "svelte-scrollto";
+	import { navigate } from "svelte-routing";
 	import { cartList } from '$store/store'
 	import CartCard from '$lib/CartCard.svelte'
   	import { Link} from "svelte-routing";
 
 	$: subTotal = $cartList.reduce((sum, item) => sum + item.price * item.quantity, 0);
 
+	let isAddressModelOpened = false;
+	let isCreditModelOpended = false;
+	let isSuccessMsgOpened = false;
+
+
+	const openPaymentModel = () =>{
+		isCreditModelOpended = !isCreditModelOpended;
+	}
+
+	const doneBuy = () => {
+		animateScroll.scrollToTop();
+		isSuccessMsgOpened = !isSuccessMsgOpened;
+		setTimeout(()=>{
+			navigate('/', { replace: true });
+		},2000)
+		$cartList = [];
+	}
+
 </script>
 
 
+{#if isSuccessMsgOpened}
+	<div class="bg-blue-300 text-center py-3">
+		Thank your for using our service.....continue shopping👋
+	</div>
+{/if}
 <div class="container mx-auto px-10 my-10">
 	<div class="flex items-start justify-between space-x-10">
 		<div class="flex flex-col w-full">
@@ -26,21 +51,79 @@
 			{/each}
 		</div>
 		<div class="w-full max-w-lg">
-			<h2 class="text-center text-2xl font-semibold">
+			<h2 class="text-center text-2xl font-semibold mb-5">
 				<span class="border-b-2 border-yellow-700 pb-1">ORDER DETAILS</span>
 			</h2>
-			<p class="my-5 text-center italic opacity-60 text-sm">Shipping costs are calculated based on product quntites you have entered</p>
-			<div class="px-[5rem] flex items-center justify-between my-4">
-				<p class="text-2xl font-semibold">Subtotal</p>
-				<p class="font-semibold">—</p>
-				<p class="text-2xl font-semibold">${Number(subTotal).toFixed(2)}</p>
+			<div class=" border border-gray-300 py-2 pb-5">
+				<p class="my-5 text-center italic opacity-60 text-sm">Shipping costs are calculated based on product quntites you have entered</p>
+				<div class="px-[5rem] flex items-center justify-between my-4">
+					<p class="text-2xl font-semibold">Subtotal</p>
+					<p class="font-semibold">—</p>
+					<p class="text-2xl font-semibold">${Number(subTotal).toFixed(2)}</p>
+				</div>
+	            <div class="px-[6rem] mt-[2rem]">
+	            	<button on:click={() => isAddressModelOpened = !isAddressModelOpened} disabled={$cartList.length === 0 ? true : false} class="disabled:bg-yellow-300 disabled:cursor-not-allowed disabled:shadow-none flex justify-center w-full py-2 mt-6 font-semibold uppercase text-medium shadow hover:shadow-lg rounded bg-[#ffa41c] ">
+	            	  <svg aria-hidden="true" data-prefix="far" data-icon="credit-card" class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M527.9 32H48.1C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48.1 48h479.8c26.6 0 48.1-21.5 48.1-48V80c0-26.5-21.5-48-48.1-48zM54.1 80h467.8c3.3 0 6 2.7 6 6v42H48.1V86c0-3.3 2.7-6 6-6zm467.8 352H54.1c-3.3 0-6-2.7-6-6V256h479.8v170c0 3.3-2.7 6-6 6zM192 332v40c0 6.6-5.4 12-12 12h-72c-6.6 0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h72c6.6 0 12 5.4 12 12zm192 0v40c0 6.6-5.4 12-12 12H236c-6.6 0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h136c6.6 0 12 5.4 12 12z"/></svg>
+	            	  <span class="ml-2">Procceed to checkout</span>
+	            	</button>
+	            </div>
 			</div>
-            <div class="px-[6rem] mt-[2rem]">
-            	<button disabled={$cartList.length === 0 ? true : false} class="disabled:bg-yellow-300 disabled:cursor-not-allowed disabled:shadow-none flex justify-center w-full py-2 mt-6 font-semibold uppercase text-medium shadow hover:shadow-lg rounded bg-[#ffa41c] ">
-            	  <svg aria-hidden="true" data-prefix="far" data-icon="credit-card" class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M527.9 32H48.1C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48.1 48h479.8c26.6 0 48.1-21.5 48.1-48V80c0-26.5-21.5-48-48.1-48zM54.1 80h467.8c3.3 0 6 2.7 6 6v42H48.1V86c0-3.3 2.7-6 6-6zm467.8 352H54.1c-3.3 0-6-2.7-6-6V256h479.8v170c0 3.3-2.7 6-6 6zM192 332v40c0 6.6-5.4 12-12 12h-72c-6.6 0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h72c6.6 0 12 5.4 12 12zm192 0v40c0 6.6-5.4 12-12 12H236c-6.6 0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h136c6.6 0 12 5.4 12 12z"/></svg>
-            	  <span class="ml-2">Procceed to checkout</span>
-            	</button>
-            </div>
+			{#if isAddressModelOpened}			
+				<div class="container mx-auto px-[4rem] my-7 border border-gray-300 py-5">
+					<h3 class="font-semibold text-lg">
+						<span class="border-b-2 border-yellow-700 pb-1">Address of Your Residence :</span>
+					</h3>
+					<form on:submit|preventDefault={openPaymentModel} class="w-full flex flex-col items-start space-y-2.5 my-3">
+						<div class="w-full flex items-start flex-col space-y-1">
+							<label class=" font-semibold text-yellow-800 " for="name">Name:</label>
+							<input required id="name" class="focus:border focus:border-[#ffa41c] focus:ring focus:ring-[#ffa41c] focus:outline-none px-2 py-1.5 w-full border border-gray-900" type="text" placeholder="Enter your name">
+						</div>
+						<div class="w-full flex items-start flex-col space-y-1">
+							<label class=" font-semibold text-yellow-800 " for="location">Location:</label>
+							<input required id="location" class="focus:border focus:border-[#ffa41c] focus:ring focus:ring-[#ffa41c] focus:outline-none px-2 py-1.5 w-full border border-gray-900" type="text" placeholder="Enter your location">
+						</div>
+						<div class="w-full flex items-start flex-col space-y-1">
+							<label class=" font-semibold text-yellow-800 " for="location">Pincode:</label>
+							<input required id="location" class="focus:border focus:border-[#ffa41c] focus:ring focus:ring-[#ffa41c] focus:outline-none px-2 py-1.5 w-full border border-gray-900" type="number" placeholder="Enter your location pincode">
+						</div>
+						<div class="w-full flex items-start flex-col space-y-1">
+							<label class=" font-semibold text-yellow-800 " for="phone">Phone Number:</label>
+							<input required id="phone" class="focus:border focus:border-[#ffa41c] focus:ring focus:ring-[#ffa41c] focus:outline-none px-2 py-1.5 w-full border border-gray-900" type="tel" placeholder="Enter your phone number">
+						</div>
+						<div class="w-full flex items-start flex-col space-y-1">
+							<label class=" font-semibold text-yellow-800 " for="email">Email:</label>
+							<input required id="email" class="focus:border focus:border-[#ffa41c] focus:ring focus:ring-[#ffa41c] focus:outline-none px-2 py-1.5 w-full border border-gray-900" type="email" placeholder="Enter your email number">
+						</div>
+						<div class="flex items-start justify-center w-full my-3">
+							<button class="py-2 px-5 font-semibold bg-[#ffa41c] mt-5">Next...Payment</button>
+						</div>
+					</form>
+				</div>
+			{/if}
+			{#if isCreditModelOpended}			
+				<div class="container mx-auto px-[4rem] my-7 border border-gray-300 py-5">
+					<h3 class="font-semibold text-lg">
+						<span class="border-b-2 border-yellow-700 pb-1">Credit Card Deatils :</span>
+					</h3>
+					<form on:submit|preventDefault={doneBuy} class="w-full flex flex-col items-start space-y-2.5 my-3">
+						<div class="w-full flex items-start flex-col space-y-1">
+							<label class=" font-semibold text-yellow-800 " for="name">Card Number:</label>
+							<input required id="name" class="focus:border focus:border-[#ffa41c] focus:ring focus:ring-[#ffa41c] focus:outline-none px-2 py-1.5 w-full border border-gray-900" type="text" placeholder="Enter your name">
+						</div>
+						<div class="w-full flex items-start flex-col space-y-1">
+							<label class=" font-semibold text-yellow-800 " for="location">Card Holder Name:</label>
+							<input required id="location" class="focus:border focus:border-[#ffa41c] focus:ring focus:ring-[#ffa41c] focus:outline-none px-2 py-1.5 w-full border border-gray-900" type="text" placeholder="Enter your location">
+						</div>
+						<div class="w-full flex items-start flex-col space-y-1">
+							<label class=" font-semibold text-yellow-800 " for="location">Expiration Date:</label>
+							<input required id="location" class="focus:border focus:border-[#ffa41c] focus:ring focus:ring-[#ffa41c] focus:outline-none px-2 py-1.5 w-full border border-gray-900" type="month" placeholder="Enter your location pincode">
+						</div>
+						<div class="flex items-start justify-center w-full my-3">
+							<button class="py-2 px-5 font-semibold bg-[#ffa41c] mt-5">Buy</button>
+						</div>
+					</form>
+				</div>
+			{/if}
 		</div>
 	</div>
 </div>
