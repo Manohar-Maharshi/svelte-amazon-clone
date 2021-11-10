f366196edbf88a9c6dfc5087b35a99fa28cae678
Manohar-Maharshi <netwitch101@gmail.com>
2021-11-10 18:33:43 +0530

    👂 onAuthState Listener

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,34 @@
 <script>
   import { Router, Link, Route } from "svelte-routing";
+  import { onMount } from 'svelte'
   import NavBar from "./lib/NavBar.svelte";
+
+	import { user } from '$store/store'
+	import { db,provider,auth } from '$store/firebase'
+ 	import { getAuth, onAuthStateChanged } from "firebase/auth";
 
   import Home from "./routes/Home.svelte";
   import Cart from "./routes/Cart.svelte";
   import WishList from "./routes/WishList.svelte";
 
+  let userInfo;
 
+
+	onMount(()=>{
+		onAuthStateChanged(auth, (user) => {
+		  if (user) {
+			userInfo = user
+		  } else {
+			userInfo = null;
+		  }
+		});
+	})
+
+	$: if(userInfo){
+		$user = userInfo;
+	}else{
+		$user = null;
+	}
 
   export let url = "";
 </script>
